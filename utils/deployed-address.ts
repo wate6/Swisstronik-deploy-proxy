acea4aa4fc373938e94c3b0c7f69525aa4c6c35c
aquawate 6 <aquawate6@gmail.com>
2024-08-03 08:20:45 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x682E79c30EF25b8b126d9082F35E6d7960395bD0'
+export const ProxyAdmin = '0x9f1e4eD11fd5a4571Afff80b3b610EbEba3F99d2'
+export const SWTRImplementation = '0x85E7f2ae047392Bd0F6A104B9740Af640E2B46c0'
     
